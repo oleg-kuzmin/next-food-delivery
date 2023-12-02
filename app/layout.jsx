4bfb359d48cb34248ff613baf0aba6aa3ce571d3
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-02 15:26:18 +0300

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+// import '@/styles/globals.scss';
+// import { Providers } from '@/redux/providers';
+// import Header from '@/components/(Header)/Header';
+// import Footer from '@/components/(Footer)/Footer';
+
+export const metadata = {
+  title: 'food-delivery',
+};
+
+export default function RootLayout(props) {
+  return (
+    // <Providers>
+      <html lang="ru">
+        <head />
+        <body className="body body_position_center" id="body">
+          {/* <Header /> */}
+          {props.children}
+          {/* <Footer /> */}
+        </body>
+      </html>
+    // </Providers>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+// import Main from '@/components/(Page Home)/Main';
+
+export default function HomePage() {
+  return <main>123</main>;
+}
