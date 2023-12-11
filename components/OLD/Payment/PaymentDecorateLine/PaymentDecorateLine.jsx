dfbc3091ddbf8d2d0ae3ae98b079ad2956838910
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-11 15:35:01 +0300

    delete Old

--- components/OLD/Payment/PaymentDecorateLine/PaymentDecorateLine.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './PaymentDecorateLine.module.scss';
-
-export default function PaymentDecorateLine() {
-  return <div className={styles.PaymentDecorateLine}></div>;
-}
--- components/OLD/Title/Title.jsx
+++
@@ -1,6 +0,0 @@
-import styles from './Title.module.scss';
-
-export default function Title({ text, className }) {
-  const classElement = className ? ` ${className}` : '';
-  return <h3 className={styles.Title + classElement}>{text}</h3>;
-}
