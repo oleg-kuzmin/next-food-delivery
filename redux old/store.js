110145d6b8c0ef89bc683546378e7fab63c5e9d9
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-13 17:44:23 +0300

    redux

---
+++ redux old/store.js
@@ -0,0 +1,12 @@
+'use client';
+
+import { configureStore } from '@reduxjs/toolkit';
+import basketReducer from '@/redux/slices/basketSlice';
+import userReducer from '@/redux/slices/userSlice';
+
+export const store = configureStore({
+  reducer: {
+    basket: basketReducer,
+    user: userReducer,
+  },
+});
