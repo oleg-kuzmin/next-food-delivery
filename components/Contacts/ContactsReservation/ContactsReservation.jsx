b4d9d4a6a81498059d416e2d68a39fdd20dfe8ea
Oleg Kuzmin <alex-dust@yandex.ru>
2023-12-15 14:43:14 +0300

    ContactsReservation

--- components/Contacts/ContactsReservation/ContactsReservation.jsx
+++ components/Contacts/ContactsReservation/ContactsReservation.jsx
@@ -10,10 +10,10 @@ export default function ContactsReservation() {
       <Link className={styles.ContactsReservation__Button + ' ui-gray outline-focus'} href="#body">
         проложить маршрут
       </Link>
-      <address className={styles.ContactsReservation__Address}>
+      <div className={styles.ContactsReservation__Address}>
         <span className={styles.ContactsReservation__Number}>+7 (917) 510-57-59</span>
         <span className={styles.ContactsReservation__Caption}>Звоните или оставляйте заявку</span>
-      </address>
+      </div>
     </div>
   );
 }
